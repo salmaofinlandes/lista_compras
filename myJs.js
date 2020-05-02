17e8a6b7cb574a91af93a9b8786c39aceac5c5d1
Mateus Pinto <salmaofinlandes@gmail.com>
2020-05-02 13:00:33 +0100

    files added

---
+++ myJs.js
@@ -0,0 +1,53 @@
+const todoInput = document.querySelector(".input");
+const todoBtn = document.querySelector(".add-btn");
+const todoList = document.querySelector(".lista");
+console.log(todoBtn, todoInput, todoList);
+
+
+todoBtn.addEventListener("click", addTodo);
+todoList.addEventListener("click", checkDelete)
+
+function checkDelete(e){
+	console.log(e.target);
+}
+
+
+function addTodo(event){
+	
+	event.preventDefault();
+	
+	//div-item
+	const div = document.createElement('div');
+	div.classList.add("div-item"); 
+	todoList.appendChild(div);
+	//li
+	const item = document.createElement('li');
+	item.innerText=todoInput.value;
+	div.appendChild(item);
+	
+	//check btn
+	const checkBtn = document.createElement('button');
+	checkBtn.innerHTML = "<i class='fas fa-check'></i>";
+	checkBtn.classList.add("check");
+	div.appendChild(checkBtn);
+	
+	//delete btn
+	const deleteBtn = document.createElement('button');
+	deleteBtn.innerHTML = "<i class='fas fa-trash'></i>";
+	deleteBtn.classList.add("delete");
+	div.appendChild(deleteBtn);
+	
+	//clear input value
+	
+	todoInput.value="";
+	
+	//set cookies 
+	
+	setCookie()
+}
+
+
+
+function setCookie(){
+	document.cookie = "value = ola"
+}
